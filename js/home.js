968c3ccba2c9b993b5cc0da2f6cb013b6ed02d76
treetrunkz <ecarl@mail.greenriver.edu>
2020-10-19 20:34:14 -0700

    html compliant

--- js/home.js
+++ js/home.js
@@ -5,3 +5,16 @@ function openForm() {
 function closeForm() {
     document.getElementById("myForm").style.display = "none";
 }
+
+// function zipcode() {
+// let input = document.getElementById("zip").value;
+// let result = document.getElementById("response");
+// console.log(input);
+//     if (input === 12345){
+//         console.log("wat s this");
+//         document.getElementById("response").innerHTML = "You are in the right way";} else {
+//         console.log("or this");
+//         document.getElementById("response").innerHTML = "You are not in the correct Zip Code for Services";
+// } result.innerHTML = result.value;
+//
+// }
